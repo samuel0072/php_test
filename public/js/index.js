0f5effb71a6099c6661aa5ad5a036cf0f4fff32c
Samuel Lima <sls@ic.ufal.br>
2021-10-26 21:26:21 -0300

    :construction: Adiciona seções sobre usuário adicionado um estilo básico adicionado mais funções para obter informações sobre o usuário botão <- precisa ser corrigido.

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,9 @@ const app = Vue.createApp({
             len_result:0,//quantidade de usuários encontrados na busca,
             found_users:[],
             usr: null,
+            repos: [],
+            show_repos : false,
+            page:1,
         }
     },
 
@@ -18,14 +21,20 @@ const app = Vue.createApp({
         var form = document.getElementById("search_bar");
         form.onsubmit = function(event) {
             event.preventDefault();
-            self.fetchUsers();//utiliza o metodo fetchUsers para buscar os usuários
+            self.fetchUsers(page=1);//utiliza o metodo fetchUsers para buscar os usuários
         }
     },
 
     methods: {
-        fetchUsers: function() {
+        fetchUsers: function(page = 1) {
+
+            if(page < 1) {
+                page = 1;
+            }
+
             var self = this;
-            var url = "search.php?q={0}".replace("{0}", self.search_term);
+            var url = "search.php?q={0}&page={1}".replace("{0}", self.search_term)
+            .replace("{1}", page);
             
             var xhr = new XMLHttpRequest();
 
@@ -38,8 +47,10 @@ const app = Vue.createApp({
                 self.found_users = response.items;
                 self.show_users = true;
                 self.show_info = false;
+                self.show_repos = false;
                 self.usr = null;
                 self.len_result = response.total_count;
+                self.page=page;
             };
             xhr.setRequestHeader("Accept", "application/json");
 
@@ -58,12 +69,40 @@ const app = Vue.createApp({
                 console.log(self.usr);
                 self.show_users = false;
                 self.show_info = true;
+                self.fetchUsrRepos(self.usr);
 
             };
             xhr.setRequestHeader("Accept", "application/json");
 
             xhr.send();
         },
+        fetchUsrRepos: function(usr) {
+            var self = this;
+            var url = "getResource.php?resrc_url={0}".replace("{0}", usr.repos_url);
+            
+            var xhr = new XMLHttpRequest();
+
+            xhr.open("GET", url);
+
+            xhr.onload = function() {
+                self.repos = JSON.parse(xhr.responseText);
+                console.log(self.repos);
+                self.show_users = false;
+                self.show_info = true;
+                self.show_repos = true;
+            };
+            xhr.setRequestHeader("Accept", "application/json");
+
+            xhr.send();
+        },
+        formatDate: function(v) {
+            return v.replace(/T|Z/g, " ");
+        },
+        back: function(){
+            show_users = true;
+            show_info = false;
+            show_repos = false;
+        }
     },
 
 });
